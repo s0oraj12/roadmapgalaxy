62cda4331bf190620d5d3693eb340a17fa032866
s0oraj12 <myselfsuraj123@gmail.com>
2025-01-03 17:00:37 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Scene from './components/galaxy/Scene';
-import RoadmapPage from './components/RoadmapPage';
+// src/App.tsx (modified)
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import HomePage from './pages/index'
+import RoadmapRoute from './pages/roadmap'
+import { useNavigationStore } from './store/navigationStore'
 
 function App() {
+  const { cursorStyle } = useNavigationStore()
+
   return (
     <Router>
-      <div className="w-full h-screen">
+      <div className="w-full h-screen" style={{ cursor: cursorStyle }}>
         <Routes>
-          <Route path="/" element={<Scene />} />
-          <Route path="/roadmap" element={<RoadmapPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/roadmap" element={<RoadmapRoute />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
-
+export default App
